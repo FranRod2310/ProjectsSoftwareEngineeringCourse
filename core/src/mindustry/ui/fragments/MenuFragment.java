0dfc5a86e62a94a3800cd6f84df7dc10ecabc911
Miguel <m.rosalino@campus.fct.unl.pt>
2025-11-18 14:28:48 +0000

    US3 - added tutorial button on the main menu

--- core/src/mindustry/ui/fragments/MenuFragment.java
+++ core/src/mindustry/ui/fragments/MenuFragment.java
@@ -205,6 +205,12 @@ public class MenuFragment{
                 desktopButtons = Seq.with(
                     new MenuButton("@play", Icon.play,
                         new MenuButton("@campaign", Icon.play, () -> checkPlay(ui.planet::show)),
+
+                        // US3 - New tutorial menu button
+                        new MenuButton("Tutorial", Icon.bookOpen, () -> {
+                            ui.showInfo("Tutorial Coming Soon!");
+                        }),
+
                         new MenuButton("@joingame", Icon.add, () -> checkPlay(ui.join::show)),
                         new MenuButton("@customgame", Icon.terrain, () -> checkPlay(ui.custom::show)),
                         new MenuButton("@loadgame", Icon.download, () -> checkPlay(ui.load::show))
