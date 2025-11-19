f19db1fb5b5cb1ef55476c303fa5657ba869e56c
Francisco <fpe.rodrigues@campus.fct.unl.pt>
2025-11-19 17:13:47 +0000

    changed tutorial to start after confirmed

--- core/src/mindustry/ui/TutorialWelcomeState.java
+++ core/src/mindustry/ui/TutorialWelcomeState.java
@@ -1,10 +1,16 @@
 package mindustry.ui;
 
 import arc.Core;
+import arc.struct.ObjectSet;
 import mindustry.Vars;
+import mindustry.content.Items;
 import mindustry.content.SectorPresets;
+import mindustry.content.TechTree;
 import mindustry.game.Rules;
+import mindustry.type.ItemStack;
 import mindustry.type.Sector;
+import mindustry.ui.dialogs.FullTextDialog;
+import mindustry.ui.dialogs.ResearchDialog;
 
 public class TutorialWelcomeState implements TutorialState {
     private Tutorial context;
@@ -16,16 +22,16 @@ public class TutorialWelcomeState implements TutorialState {
 
     @Override
     public void enter() {
-
-
-        // join tutorial map as a custom game, makes sure map is empty when loading
-        Vars.control.playMap(Vars.maps.loadInternalMap("groundZero"), new Rules());
-
         // welcome msg
         Core.app.post(() -> {
-            Vars.ui.showInfo("Welcome to the Tutorial!\n\nClick 'OK' to begin.");
+            Vars.ui.showConfirm("Welcome to the Tutorial!\n\nClick 'OK' to begin.", () -> {
+                // when confirmed, join tutorial map as a custom game, makes sure map is empty when loading
+                Rules rules = new Rules();
+                //no starting resources
+                rules.loadout = ItemStack.list();
+                Vars.control.playMap(Vars.maps.loadInternalMap("groundZero"), rules);
+            });
         });
-
     }
 
     @Override
