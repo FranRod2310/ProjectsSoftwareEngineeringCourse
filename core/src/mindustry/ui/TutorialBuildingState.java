dc3417b4bf9e298e277bd7356a7686bf664f905e
Miguel <m.rosalino@campus.fct.unl.pt>
2025-11-20 16:50:16 +0000

    US3 - updated WelcomeState with copper ore location, removed original tutorial objectives, started BuildingTutorial

--- core/src/mindustry/ui/TutorialBuildingState.java
+++ core/src/mindustry/ui/TutorialBuildingState.java
@@ -1,15 +1,50 @@
 package mindustry.ui;
 
+import arc.Events;
+import arc.math.Mathf;
+import mindustry.Vars;
+import mindustry.content.Items;
+import mindustry.game.EventType;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
+import mindustry.world.Tile;
+
+import static mindustry.Vars.tilesize;
+
 public class TutorialBuildingState implements TutorialState {
+
+    private Tutorial context;
+    private Boolean active = false;
+
+
     @Override
     public void setContext(Tutorial context) {
-
+        this.context = context;
     }
 
     @Override
     public void enter() {
+        active = true;
+        Vars.ui.announce("Objective: Build a Mechanical Drill");
 
+        // get tile with copper ore
+        Tile closestOre = Vars.indexer.findClosestOre(Vars.player.x, Vars.player.y, Items.copper);
+
+        Events.run(EventType.Trigger.draw, () -> {
+            if (active && closestOre != null) {
+                // draw a pulsing square around the target
+                Drawf.square(closestOre.worldx(), closestOre.worldy(),
+                        tilesize * 1.5f + Mathf.absin(10f, 2f), Pal.accent);
+
+                // draw an arrow pointing down to it
+                Drawf.arrow(closestOre.worldx(), closestOre.worldy() + 40f,
+                        closestOre.worldx(), closestOre.worldy() + 8f,
+                        tilesize + Mathf.absin(10f, 2f), 4f, Pal.accent);
+            }
+        });
     }
+
+
 
     @Override
     public void update() {
--- core/src/mindustry/ui/TutorialWelcomeState.java
+++ core/src/mindustry/ui/TutorialWelcomeState.java
@@ -3,9 +3,11 @@ package mindustry.ui;
 import arc.Core;
 import arc.struct.ObjectSet;
 import mindustry.Vars;
+import mindustry.content.Blocks;
 import mindustry.content.Items;
 import mindustry.content.SectorPresets;
 import mindustry.content.TechTree;
+import mindustry.game.MapObjectives;
 import mindustry.game.Rules;
 import mindustry.type.ItemStack;
 import mindustry.type.Sector;
@@ -14,28 +16,69 @@ import mindustry.ui.dialogs.ResearchDialog;
 
 public class TutorialWelcomeState implements TutorialState {
     private Tutorial context;
+    private boolean rulesApplied = false;
 
     @Override
     public void setContext(Tutorial context) {
         this.context = context;
     }
 
+
+    /**
+     * we have to use playSector, playMap creates a custom game which messes with the tutorial
+     */
     @Override
     public void enter() {
-        // welcome msg
         Core.app.post(() -> {
             Vars.ui.showConfirm("Welcome to the Tutorial!\n\nClick 'OK' to begin.", () -> {
-                // when confirmed, join tutorial map as a custom game, makes sure map is empty when loading
-                Rules rules = new Rules();
-                //no starting resources
-                rules.loadout = ItemStack.list();
-                Vars.control.playMap(Vars.maps.loadInternalMap("groundZero"), rules);
+
+                Sector sector = SectorPresets.groundZero.sector;
+
+                // reset save data for tutorial
+                // otherwise might load groundZero progress from main game
+                if (sector.save != null) {
+                    sector.save.delete();
+                    sector.save = null;
+                }
+
+                // lock research - list of blocks to unlock in tutorial
+                // don't unlock the whole tree, since it deletes game progress from other saves
+                // instead, just unlock the blocks we need for the tutorial since they'll be unlocked by the end anyway
+                Blocks.mechanicalDrill.clearUnlock();
+                Blocks.duo.clearUnlock();
+                Blocks.copperWall.clearUnlock();
+
+                Vars.control.playSector(sector);
             });
         });
     }
 
     @Override
-    public void update() {}
+    public void update() {
+        // wait for tutorial start
+        if (Vars.state.isGame()) {
+
+            // custom rules for tutorial:
+            if (!rulesApplied) {
+
+                // remove wave timer
+                Vars.state.rules.waveTimer = false;
+
+                // remove enemy spawning
+                Vars.state.rules.waves = false;
+
+                // remove original tutorial objectives in groundZero ONLY for tutorial mode
+                Vars.state.rules.objectives = new MapObjectives();
+
+
+            }
+
+            // wait for any info box to close
+            if (!Core.scene.hasDialog()) {
+                context.changeState(new TutorialBuildingState());
+            }
+        }
+    }
 
     @Override
     public void exit() {}
